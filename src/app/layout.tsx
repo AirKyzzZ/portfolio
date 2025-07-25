70b69c0c10092c4fc56df0d14359eb3e787fbb27
AirKyzzZ <airkyzzz.jiji@gmail.com>
2025-07-25 12:30:23 +0200

    feat(navbar): floating navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { FloatingNavBar } from "@/components/ui/floating-navbar-prop";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <FloatingNavBar />
         {children}
       </body>
     </html>
